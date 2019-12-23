26cb62258a6a91973ee149c1812cada979cdda60
yangseungbin <tanja1206@naver.com>
2019-12-23 19:56:29 +0900

    middleware

---
+++ Backend/middlewares/auth.js
@@ -0,0 +1,28 @@
+const jwt = require('jsonwebtoken');
+
+const authMiddleware = (req, res, next) => {
+    const token = req.headers['x-access-token'];
+
+    if (!token) {
+        res.status(403).json({ message: 'not logged in' });
+    }
+
+    const p = new Promise((resolve, reject) => {
+        jwt.verify(token, req.app.get('jwt-secret'), (err, decoded) => {
+            if (err) reject(err);
+            resolve(decoded);
+        });
+    });
+
+    const onError = (err) => {
+        res.status(403).json(err.message);
+    }
+
+    p.then((decoded) => {
+        req.decoded = decoded;
+        next();
+    })
+    .catch(onError)
+}
+
+module.exports = authMiddleware;

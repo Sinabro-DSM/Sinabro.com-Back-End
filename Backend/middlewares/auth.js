1633a5f6ed4507b2cd4b383e8fee382336e114f3
seungbin <tanja1206@naver.com>
2020-05-06 16:37:03 +0900

    middleware

--- Backend/middlewares/auth.js
+++ Backend/middlewares/auth.js
@@ -1,7 +1,7 @@
 const jwt = require('jsonwebtoken');
 
 const authMiddleware = (req, res, next) => {
-    const token = req.headers['x-access-token'];
+    const token = req.headers['access_token'];
 
     if (!token) {
         res.status(403).json({ message: 'not logged in' });

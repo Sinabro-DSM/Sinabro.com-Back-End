91d8ae423eaba323f85d7103c5dba9a20b082884
seungbin <tanja1206@naver.com>
2020-05-06 16:37:14 +0900

    model

--- Backend/models/post.js
+++ Backend/models/post.js
@@ -5,12 +5,35 @@ const Schema = mongoose.Schema;
 const Post = new Schema({
     title: String,
     content: String,
-    post: String,
+    file: String,
     category: String,
     date: {
         type: Date,
         default: Date.now
     }
 });
+
+Post.statics.create = function(title, content, file, category) {
+    const post = new this({
+        title,
+        content,
+        file,
+        category
+    });
+
+    return post.save();
+}
+
+Post.statics.findAllCategory = function(category) {
+    return this.find({
+        category
+    }).exec();
+}
+
+Post.statics.findOneById = function(_id) {
+    return this.findOne({
+        _id
+    }).exec();
+}
 
 module.exports = mongoose.model('Post', Post);
